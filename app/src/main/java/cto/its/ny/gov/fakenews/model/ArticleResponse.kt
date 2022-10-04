d00b04bc6aac0362d9a92ede33e3f5f82b8f17c7
montyJordanIII <montyJordanIII@users.noreply.github.com>
2022-10-04 15:21:15 -0400

    coroutine and retrofit with recycler adapter after many tries in other apps

--- app/src/main/java/cto/its/ny/gov/fakenews/model/ArticleResponse.kt
+++ app/src/main/java/cto/its/ny/gov/fakenews/model/ArticleResponse.kt
@@ -1,12 +1,14 @@
 package cto.its.ny.gov.fakenews.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
-
+@Parcelize
 data class ArticleResponse (
 
     @SerializedName("status"       ) var status       : String?             = null,
     @SerializedName("totalResults" ) var totalResults : Int?                = null,
-    @SerializedName("articles"     ) var articles     : ArrayList<Article> = arrayListOf()
+    @SerializedName("articles"     ) var articles     : List<Article> = arrayListOf()
 
-)
+) : Parcelable
